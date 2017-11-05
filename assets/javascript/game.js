639b841f585b0a0e9d457a0eea8dd41891096401
Mary Hall <mh6uj@students-MBP.home>
2017-11-05 16:52:45 -0500

    initial commit files

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+//Rock legends arrary. This array holds the words for the game.
+
+var rockLegendsArray=["Stevie Nicks", "Aerosmith", "Fats Domino", "Elvis", "Lynrd Skynrd", "Eagles"]
+
+//Word randomizer
+
